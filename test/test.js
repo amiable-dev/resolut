67f3a3f958d15ba6e99ed67c783819b5b4a46cba
Chris Joseph <chris@frugal.me.uk>
2016-04-25 17:46:33 +0100

    fizzbuzz function in server.js to test coverage

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var assert = require('chai').assert;
+/* var assert = require('chai').assert;
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
@@ -6,4 +6,29 @@ describe('Array', function() {
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
+});*/
+
+var assert = require('assert');
+var fizzbuzz = require('../');
+ 
+describe('fizzbuzz', function() {
+ it('returns null when passed a non-number', function() {
+   assert.equal(fizzbuzz('abc'), null);
+ });
+ 
+ it('returns fizzbuzz when divisible by 15', function() {
+   assert.equal(fizzbuzz(45), 'fizzbuzz');
+ });
+ 
+ it('returns fizz when divisible by 3 but not 5', function() {
+   assert.equal(fizzbuzz(6), 'fizz');
+ });
+ 
+ it('returns buzz when divisble by 5 but not 3', function() {
+   assert.equal(fizzbuzz(10), 'buzz');
+ });
+ 
+ it('returns input otherwise', function() {
+   assert.equal(fizzbuzz(7), '7');
+ });
 });
